827497d16e0ca2b6404f167b46431349360e4a0f
chaosannals <chaosannals@outlook.com>
2024-05-03 10:31:02 +0800

    log.

--- main.go
+++ main.go
@@ -32,11 +32,17 @@ func route(
 	e.GET("/", indexController.Index)
 	employeeGroup := e.Group("employee", middleware.Gzip())
 	employeeGroup.GET("/detail", employeeController.Detail)
+
 	return e
 }
 
 func main() {
 	app, err := cjungo.NewApplication(func(c *dig.Container) error {
+		// 加载日志配置
+		if err := c.Provide(cjungo.LoadLoggerConfFromEnv); err != nil {
+			return err
+		}
+		// 加载服务器配置
 		if err := c.Provide(cjungo.LoadHttpServerConfFromEnv); err != nil {
 			return err
 		}
